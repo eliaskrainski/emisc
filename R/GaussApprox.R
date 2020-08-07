e420173ff7857355d2eee09f112fcc5455968a94
krainski <eliask@eliask-laptop.math.ntnu.no>
2020-08-07 17:53:20 -0300

    update GA

--- R/GaussApprox.R
+++ R/GaussApprox.R
@@ -1,40 +1,58 @@
-#' @title GaussApprox
-#' \code{GaussApprox} computes the Gaussian approximation
+#' Computes the Gaussian approximation
 #' given the likelihood function, design matrix and
 #' the prior precision matrix.
-#' @param f the function that computes the likelihood
+#' @param f a function that computes the likelihood
 #' @param A the design matrix
 #' @param Q the prior precision matrix
 #' @param k the number of iterations
 #' @param h the change around the avaluation point
+#' @param x vector with the initial value
 #' @param ... additional arguments to f
 #' @examples
+#' ## Consider confirmed cases of COVID19 in NS
 #' data(NSCcases)
+#' with(NSCcases, plot(day, cases, pch=8))
+#' ### likelihood
 #' llf <- function(x)
-#'     dgamma(cases, exp(x), log=TRUE)
+#'     dpois(NSCcases$cases, exp(x), log=TRUE)
 #' n <- length(NSCcases$cases)
 #' library(Matrix)
-#' Rt <- crossprod(diff(Diagonal(n)))
-#' tau <- 1500
+#' ## precision structure matrix of a RW2 model
+#' Rt <- crossprod(diff(Diagonal(n), differences=2))
+#' tau <- 5 ## a reasonable value for the precision
+#' ### perform GA
 #' ga <- GaussApprox(llf, Diagonal(n), Rt*tau)
-#' with(NSCcases, plot(day, cases, pch=8))
 #' lines(NSCases$day, ga$mu)
+#'
+#' ## Consider Tokyo data from INLA package
+#' data('Tokyo', package='INLA')
+#' if (any(ls()=='Tokyo')) {
+#'   llb <- function(x)
+#'      dbinom(Tokyo$y, Tokyo$n, plogis(x), log=TRUE)
+#'   nt <- nrow(Tokyo)
+#'   Rt <- crossprod(diff(Diagonal(n), differences=2))
+#'   taup <- 15000
+#'   gab <- GaussApprox(llb, Diagonal(n), Rt*taup)
+#'   with(Tokyo, plot(time, y/n, pch=19))
+#'   lines(Tokyo$time, plogis(gab$mu))
+#' }
 GaussApprox <- function(f, A, Q, k=5,
                         h=.Machine$double.eps^0.2,
-                        ...) {
-    b <- double(ncol(A))
+                        x=NULL, ...) {
+    if (is.null(x))
+        x <- double(ncol(A))
     for (j in 1:k) {
-        fx <- f(b)
-        fa  <- f(b - h)
-        fb  <- f(b + h)
+        fx <- f(x)
+        fa  <- f(x - h)
+        fb  <- f(x + h)
         cc <- (2*fx -fa -fb)/(h^2)
         cc[cc<0] <- 0
-        xx <- (fb - fa)/(2*h) + b*cc
+        xx <- (fb - fa)/(2*h) + x*cc
         Qn <- Q + Diagonal(x=cc)
         L <- chol(Qn)
         new <- drop(solve(L, solve(t(L), xx)))
-        b <- new
+        x <- new
     }
-    return(list(mu=b, bb=xx, cc=cc, Q=Qn, L=L,
+    return(list(mu=x, bb=xx, cc=cc, Q=Qn, L=L,
                 sldL=sum(log(diag(L)))))
 }
