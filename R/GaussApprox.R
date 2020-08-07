ad4934aa5d11508183b397d24dc3d04beb473968
krainski <eliask@eliask-laptop.math.ntnu.no>
2020-08-07 13:01:09 -0300

    add GaussApprox.R

---
+++ R/GaussApprox.R
@@ -0,0 +1,42 @@
+#' @title Gaussian Approximation
+#' \code{GaussApprox} computes the Gaussian approximation
+#' given the likelihood function, design matrix and
+#' the prior precision matrix. 
+#' @param f the function that computes the likelihood 
+#' @param A the design matrix
+#' @param Q the prior precision matrix
+#' @param k the number of iterations
+#' @param h the change around the avaluation point
+#' @param ... additional arguments to f
+#' @examples
+#' data(Tokyo, package='INLA')
+#' if (any(ls()=='Tokyo')) {
+#'   llfbt <- function(x)
+#'     dbinom(Tokyo$y, Tokyo$n, plogis(x), log=TRUE)
+#'   n <- nrow(Tokyo)
+#'   library(Matrix)
+#'   Rt <- crossprod(diff(Diagonal(n)))
+#'   tau <- 14500
+#'   ga <- GaussApprox(llfbt, Diagonal(n), Rt*tau)
+#'   with(Tokyo, plot(time, y/n, pch=8))
+#'   lines(ga$mu)
+#' }
+GaussApprox <- function(f, A, Q, k=5,
+                        h=.Machine$double.eps^0.2,
+                        ...) {
+    b <- double(ncol(A))
+    for (j in 1:k) {
+        fx <- f(b)
+        fa  <- f(b - h)
+        fb  <- f(b + h)
+        cc <- (2*fx -fa -fb)/(h^2)
+        cc[cc<0] <- 0
+        xx <- (fb - fa)/(2*h) + b*cc
+        Qn <- Q + Diagonal(x=cc)
+        L <- chol(Qn)
+        new <- drop(solve(L, solve(t(L), xx))) 
+        b <- new
+    }
+    return(list(mu=b, bb=xx, cc=cc, Q=Qn, L=L,
+                sldL=sum(log(diag(L)))))
+}
